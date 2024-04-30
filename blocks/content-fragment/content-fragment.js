e1f0ad1f5c893e575711954b4e07a3ea30711a40
raulugarte <63106469+raulugarte@users.noreply.github.com>
2024-04-30 23:08:47 +0200

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -35,7 +35,7 @@ fetch(aem + '/graphql/execute.json/aem-demo-assets/adventure-by-slug;slug=' + sl
 .then(response => response.json())
 .then(response => {
 
-const backgroundImage = response.data.adventureList.items[0].primaryImage._dynamicUrl;
+const backgroundImage = response.data.adventureList.items[0].primaryImage._publishUrl;
 document.getElementById(adventureDiv.id).innerHTML = "<section><img src=" + backgroundImage + "></section>";  
 
 const adventureTitle = response.data.adventureList.items[0].title;
