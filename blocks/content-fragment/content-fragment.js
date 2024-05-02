279153cc6843ea505661cc1e3a09aff7f708cca7
raulugarte <63106469+raulugarte@users.noreply.github.com>
2024-05-02 16:00:06 +0200

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -31,7 +31,9 @@ fetch(aem + '/graphql/execute.json/aem-demo-assets/adventure-by-slug;slug=' + sl
 .then(response => response.json())
 .then(response => {
 
-const backgroundImage = response.data.adventureList.items[0].primaryImage._dynamicUrl;
+  console.log('response.data.adventureList.items');
+  
+const backgroundImage = response.data.adventureList.items[0].primaryImage._publishUrl;
 document.getElementById(adventureDiv.id).innerHTML = "<section><img src=" + backgroundImage + "></section>";  
 
 const adventureTitle = response.data.adventureList.items[0].title;
